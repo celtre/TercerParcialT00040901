7bbf56b0dae7b2c067b2e32be77fd9a5bc66c1b2
celtre <t00040901@utbvirtual.edu.co>
2016-11-28 12:26:49 -0500

    Segundo push

---
+++ app/src/main/java/com/example/labsoftware13/tercerparcialt00040901/PeleaActivity.java
@@ -0,0 +1,13 @@
+package com.example.labsoftware13.tercerparcialt00040901;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PeleaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pelea);
+    }
+}
